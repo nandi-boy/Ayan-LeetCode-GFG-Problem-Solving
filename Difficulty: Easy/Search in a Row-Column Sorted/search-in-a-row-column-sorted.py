b1d26f60e37e9e179c6643b45aafdef4be2e6838
Ayan Nandi <ayannandi2004@gmail.com>
2026-05-12 11:35:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search in a Row-Column Sorted/search-in-a-row-column-sorted.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+class Solution:
+	def matSearch(self, mat, x):
+	    
+		for i in range (len(mat)):
+            for j in range(len(mat[i])):
+                if mat[i][j] == x:
+                    return True
+        else:
+            return False
