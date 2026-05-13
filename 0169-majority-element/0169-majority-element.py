1d07162c13a9358d2136f26ea5e80508d3e3bf96
Ayan Nandi <ayannandi2004@gmail.com>
2026-05-13 11:33:24 +0530

    Time: 5 ms (73.9%), Space: 13.5 MB (73.69%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,13 +1,24 @@
 class Solution(object):
     def majorityElement(self, nums):
-        dict = {}
-        threshold = len(nums)//2
-        for i in nums:
-            if i in dict :
-                dict[i]+=1
-            else :
-                dict[i] = 1
+        # dict = {}
+        # threshold = len(nums)//2
+        # for i in nums:
+        #     if i in dict :
+        #         dict[i]+=1
+        #     else :
+        #         dict[i] = 1
                 
-            if dict[i] > threshold :
-                return i
-        
+        #     if dict[i] > threshold :
+        #         return i
+        candidate = None
+        count = 0
+        for i in nums :
+            if count == 0:
+                candidate = i
+                count = 1
+            elif candidate == i:
+                count+=1
+            else:
+                count -= 1
+            
+        return candidate
