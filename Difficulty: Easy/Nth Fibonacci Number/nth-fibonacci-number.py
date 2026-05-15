653d10e130c7d25a1a346c1c551e6eac48a16ea6
Ayan Nandi <ayannandi2004@gmail.com>
2026-05-15 15:54:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def nthFibonacci(self, n: int) -> int:
+        if n==0:
+            return 0
+        elif n==1:
+            return 1
+        elif n==2:
+            return 1
+        else :
+            return self.nthFibonacci(n-1) + self.nthFibonacci(n-2)
+        
