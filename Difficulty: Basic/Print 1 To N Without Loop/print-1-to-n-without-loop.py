87ade62e50fe8a25bc8d4ea52426520fcce856a2
Ayan Nandi <ayannandi2004@gmail.com>
2026-05-15 15:45:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print 1 To N Without Loop/print-1-to-n-without-loop.py
@@ -0,0 +1,10 @@
+class Solution:
+    def printNos(self, n):
+        if n==0:
+            return 0 
+        self.printNos(n-1)
+        print(n,end=' ')
+        
+        
+            
+            
