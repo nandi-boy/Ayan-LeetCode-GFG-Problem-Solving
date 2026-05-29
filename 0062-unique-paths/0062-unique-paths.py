4c5cc3841e023a4859ed622cb03fb626a4f6ebf2
Ayan Nandi <ayannandi2004@gmail.com>
2026-05-29 15:54:58 +0530

    Time: 0 ms (100%), Space: 19.2 MB (94.03%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,9 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        row = [1] * n
+        
+        for r in range(1, m):
+            for c in range(1, n):
+                row[c] += row[c - 1]
+        return row[-1]
+        
