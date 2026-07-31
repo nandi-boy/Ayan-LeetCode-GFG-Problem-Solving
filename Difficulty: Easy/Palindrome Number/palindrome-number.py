75a5c268e02ed1a3c8745066bb35a64ff64a7829
Ayan Nandi <ayannandi2004@gmail.com>
2026-07-31 23:06:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Palindrome Number/palindrome-number.py
+++ Difficulty: Easy/Palindrome Number/palindrome-number.py
@@ -1,15 +1,18 @@
 class Solution:
     def isPalindrome(self, n):
-        x = n
-        rev = 0
-        n = abs(n)
-        while n>0:
-            d = n%10
-            rev = rev*10+d
-            n = n//10
-        if x<0:
-            rev = -rev
+        org = n
+        n=abs(n)
+    	rev = 0
+    	while n>0:
+    	    d = n%10
+    		rev = rev*10+d
+    		n = n//10
         
-        if x==rev:
-            return True
-        
+    	if org <0:
+    	    rev=-rev
+		if rev == org:
+		    return True
+    	else:
+    		 return False
+		
+		
