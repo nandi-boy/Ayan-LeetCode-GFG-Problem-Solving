2b03e419d288e9ab883b919b6f2ab7c0113ad76f
Ayan Nandi <ayannandi2004@gmail.com>
2026-08-01 20:04:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Leap Year/leap-year.py
+++ Difficulty: Basic/Leap Year/leap-year.py
@@ -2,12 +2,9 @@ class Solution:
     def checkYear (self, n):
         if n%400 == 0:
             return True
-            
-        elif n%100!=0:
-            if n%4==0:
-                return True
-                
-        else :
+        elif n%100 != 0 and n%4 == 0:
+            return True
+        else:
             return False
         
-     
+        
