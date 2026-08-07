70d7edf26f740602d4202d07072c724353c0a111
Ayan Nandi <ayannandi2004@gmail.com>
2026-08-07 22:07:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Decimal to binary/decimal-to-binary.py
@@ -0,0 +1,11 @@
+class Solution:
+    def decToBinary(self, n):
+        if n == 0:
+            return "0"
+        res = ''
+        while n>0:
+            res=str(n%2)+res
+            n=n//2
+        
+        return res
+        
